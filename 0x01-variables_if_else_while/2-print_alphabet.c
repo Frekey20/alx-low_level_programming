43c9bcbd76c2249f432abfcbccfe5204e0ebb7d4
frekey20 <frekeycolz@gmail.com>
2022-12-11 03:20:42 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <ssttdio.h>
+
+/**
+*Main- where the main codes are left
+*Return: Always 0
+*/
+int main(void)
+{
+	char lower_case;
+
+	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+	{
+		putchar(lower_case);
+	}
+	putchar("\n")
+	return (0);
+}
