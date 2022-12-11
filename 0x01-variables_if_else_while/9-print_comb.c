7b15e08c12020e6786bce972dd8dca01f526fe8b
frekey20 <frekeycolz@gmail.com>
2022-12-11 07:22:18 -0800

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - block code
+* Return: always 0
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+
+}
