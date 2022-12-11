b9c5b6002c595d9906d255b8d92e76a960bcaab9
frekey20 <frekeycolz@gmail.com>
2022-12-11 08:58:51 -0800

    task (

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
-
+#include <ctype.h>
 /**
 * main - block code
+*Description: program that prints two digits.
 * Return: always 0
 */
-
 int main(void)
 {
-	int n;
+	int digit;
 
-	for (n = 0; n < 10; n++)
+	for (digit = '0'; digit <= '9'; digit++)
 	{
-		putchar((n % 10) + '0');
+		putchar(digit);
+		if (digit == '9')
+			continue;
+
 		putchar(',');
+		putchar(' ');
+
 	}
 	putchar('\n');
 
