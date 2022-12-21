5f8c5588b1cb6bf6449fc267600b6c7ed5390a58
frekey20 <frekeycolz@gmail.com>
2022-12-21 14:11:01 -0800

    task 0

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <stdio.h>
+/**
+*void prototypes()
+*int prototypes()
+*char prototypes()
+*/
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -11,4 +16,4 @@ char *cap_string(char *);
 char *leet(char *a);
 int _putchar(char c);
 
-#endif
+#endif /*MAIN_H*/
