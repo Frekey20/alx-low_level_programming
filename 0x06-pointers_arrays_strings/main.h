c32e8aecc6641557fe02b4532cfe497fda6675e5
frekey20 <frekeycolz@gmail.com>
2022-12-22 15:02:07 -0800

    task 10

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -15,5 +15,7 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *a);
 int _putchar(char c);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif /*MAIN_H*/
