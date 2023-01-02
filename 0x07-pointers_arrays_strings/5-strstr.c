e6a3e05f8252f2cacbb5bafdbf7a53bcba27af59
frekey20 <frekeycolz@gmail.com>
2023-01-02 15:56:50 -0800

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,21 +1,40 @@
 #include "main.h"
+#define NULL 0
+
 /**
-*print_chessboard - print chessboard given set 2D array
-*@a: 2D array
+*   _strstr - locate and return pointer to first occurence of substring
+*    @haystack: string to search
+*     @needle: target substring to search for
+*  Return: pointer to index of string at first occurence of whole substring
 */
 
-void print_chessboard(char (*a)[8])
+char *_strstr(char *haystack, char *needle)
 {
+	int i = 0, j, x;
 
-	int row;
-	int column;
+	if (needle[0] == '\0')
+	return (haystack);
 
-	for (row = 0; row < 8; row++)
+	while (haystack[i] != '\0') /* iterate through haystack */
+{
+/* if a byte matches first char of needle */
+/* interate through needle until match ends */
+	if (haystack[i] == needle[0])
 	{
-	for (column = 0; column < 8; column++)
-	_putchar(a[row][column]);
-	_putchar('\n');
+	x = i, j = 0;
+	while (needle[j] != '\0')
+	{
+	if (haystack[x] == needle[j])
+	x++, j++;
+	else
+		break;
+	} /* if matched throughout, return haystack */
+	if (needle[j] == '\0')
+	{
+	return (haystack + i);
 	}
-	return (0);
-
+	}
+	i++;
+	}
+	return (NULL); /* No match */
 }
