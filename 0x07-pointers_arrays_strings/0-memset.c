1ef53cff8d625f66a03e0e14fe5b21f775a596df
frekey20 <frekeycolz@gmail.com>
2023-01-02 07:57:41 -0800

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-*_memset - fills n bytes of memory with a constant value
-*@n: is the number of counts
-*@s: pointer to memory area
-*@b: constant value
-*return: memory area
-*/
+ *  _memset - fill n bytes of memory with a constant value
+ *  @s: pointer to memory area
+ *  @b: constant value
+ *  @n: number of bytes to fill
+ *  Return: memory area
+ */
 
 char *_memset(char *s, char b, unsigned int n)
 {
