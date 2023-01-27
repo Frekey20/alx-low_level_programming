bafd1c82fc225ca3b95c6ab8ea65b1b7d569c284
frekey20 <frekeycolz@gmail.com>
2023-01-27 07:27:26 -0800

    task 1

--- 0x12-singly_linked_lists/main.c
+++ 0x12-singly_linked_lists/main.c
@@ -1,40 +1,24 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "lists.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    list_t *head;
-    list_t *new;
-    list_t hello = {"World", 5, NULL};
-    size_t n;
-
-    head = &hello;
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    new->str = strdup("Hello");
-    new->len = 5;
-    new->next = head;
-    head = new;
-    n = print_list(head);
-    printf("-> %lu elements\n", n);
-
-    printf("\n");
-    free(new->str);
-    new->str = NULL;
-    n = print_list(head);
-    printf("-> %lu elements\n", n);
-
-    free(new);
-    return (0);
-}
+  1 #include <stdlib.h>
+  2 #include <string.h>
+  3 #include <stdio.h>
+  4 #include "lists.h"
+  5
+  6 /**
+  7  * main - check the code
+  8  *
+  9  * Return: Always 0.
+ 10  */
+ 11 int main(void)
+ 12 {
+ 13     list_t *head;
+ 14     list_t *new;
+ 15     list_t hello = {"World", 5, NULL};
+ 16     size_t n;
+ 17
+ 18     head = &hello;
+ 19     new = malloc(sizeof(list_t));
+ 20     if (new == NULL)
+ 21     {
+ 22         printf("Error\n");
+ 23         return (1);
+-- INSERT --                
